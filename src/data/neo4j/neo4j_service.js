b3e13b66d95c262e3251e8f1ee026490f0a4b71a
Vuong Vu <vuongvu061101@gmail.com>
2023-12-18 05:03:39 +0700

    updated cloud database connection

--- src/data/neo4j/neo4j_service.js
+++ src/data/neo4j/neo4j_service.js
@@ -24,9 +24,10 @@ class Neo4jService {
     }
 
     #createDriver() {
-        const url = 'bolt://localhost:7687'
+        const url = 'neo4j+s://897417de.databases.neo4j.io'
+        // const url = 'bolt://localhost:7687'
         const username = 'neo4j'
-        const password = '0365466031'
+        const password = '8dZ5aKvfqwvu8Jye2BbTGQEBBzDvOqYHnomp83MJXJo'
         return neo4j.driver(
             url,
             neo4j.auth.basic(username, password),
